2606a6ef77170391c06c706bf039a86acdb24d4b
Welder Victor <weldersalvadorvictor12@hotmail.com>
2023-08-12 19:19:20 -0300

    price component

--- projetos/age-calculator-app-main/script.js
+++ projetos/age-calculator-app-main/script.js
@@ -1,13 +1,36 @@
+const date = new Date()
+const year = date.getFullYear()
+const month = date.getMonth()
+const day = date.getDay()
+const fday = document.getElementById('day')
+const fyear = document.getElementById('year')
+const fmonth = document.getElementById('month')
+const textDay = document.getElementById('d1')
+const textMonth = document.getElementById('d2')
+const TextYear = document.getElementById('d3')
+
 function calculate(){
-    let date = new Date()
-    let year = date.getFullYear()
-    let month = date.getMonth()
-    let day = date.getDay() 
-    let fday = document.getElementByName('day')
-    let fmonth = document.getElementByName('month')
-    let fyear = document.getElementByName('year')
-    let container = document.getElementsByClassName('container')
-    if (container.value.length == 0){
+    if (fday.value.length == 0){
         alert('ERROR')
+    }else if(fday.value > 31 || fday.value <= 0){
+        alert("Must be a valid day")
+    }else if (fmonth.value.length == 0 ){
+        alert("ERROR")
+    }else if (fmonth.value > 12 || fmonth.value <= 0){
+        alert("Must be a valid month")
+    }else if (fyear.value.length == 0){
+        alert("ERROR")
+    }else if (fyear.value > year){
+        alert("The year must be in the past")
+    }else{
+        
     }
+    //const yearValue = ((year - fyear.value) - 1)
+    //const monthValue = (month - fmonth.value)
+    //const resOne = document.getElementById('--1')
+    //const resTwo = document.getElementById('--2')
+    //resTwo.innerHTML = monthValue
+    //resOne.innerHTML = yearValue
+    
+    
 }
